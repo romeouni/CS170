a833ac1b64ea696f640bba16c59705fb59934160
rahululman <rahul.ulman@gmail.com>
2021-11-29 02:51:33 -0500

    Fixing Filter.java

--- HW05/Filter.java
+++ HW05/Filter.java
@@ -1,4 +1,31 @@
+import java.util.regex.*;
 public class Filter{
 
-  public static void 
+  public static void main(String[] args){
+    String input = "A duck was sailing on a ship shipping whole wheat bread. Duck that SHIP!!!";
+    String[] list = new String[]{"duck", "ship", "whole"};
+    System.out.println(input);
+    System.out.println(swearFilter(list, input));
+  }
+
+  public static String swearFilter(String[] swear, String text){
+    String check;
+    String temp = "";
+    String result = text;
+    for(int i = 0; i < swear.length; i++){
+      if(swear[i].length()>text.length()){
+        continue;
+      }
+
+      check = "(?i)(" + swear[i].charAt(0) + ")(" + swear[i].substring(1,
+        swear[i].length()-1) + ")(" + swear[i].charAt(swear[i].length()-1) + ")";
+      for(int j = 0; j < swear[i].length()-2; j++){
+        temp += "*";
+      }
+
+      result = result.replaceAll(check, "$1"+temp+"$3");
+      temp = "";
+    }
+    return result;
+  }
 }
--- qtest/qtest-windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd %~dp0
-java -cp ".;lib/*" -Djava.security.policy=lib/client.policy client.QTestLauncher server=qtest2.mathcs.emory.edu %* >nul 2>&1
-
